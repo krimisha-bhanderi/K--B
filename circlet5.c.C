6df69b277d0affb37940096371712cfceecb75ac
krimisha-bhanderi <krimisha025@gmail.com>
2023-06-14 03:27:49 +0100

    circlet

---
+++ circlet5.c.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+      int i,j,k,n;
+      clrscr();
+      for(i=5;i>=1;i--)
+      {
+	 for(k=1;k<i;k++)
+	 {
+	   printf(" ");
+	 }
+	 for(j=i;j<=5;j++)
+	 {
+	   printf("%d",j);
+	 }
+	 for(n=4;n>=i;n--)
+	 {
+	   printf("%d",n);
+	 }
+	 printf("\n");
+	 }
+
+	 getch();
+	 }
+
